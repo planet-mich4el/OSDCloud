7d5d0003fddf3fbc169307bf1e05e8a295e9941c
Michael Kuczynski <Michael.Kuczynski@assaabloy.com>
2024-03-15 16:17:42 +0100

    Update StartURL.ps1

--- StartURL.ps1
+++ StartURL.ps1
@@ -1,4 +1,14 @@
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 Start-Sleep -Seconds 5
 
-Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 23H2 -OSEdition Enterprise -OSLanguage en-us -OSLicense Retail
+Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force
+
+Write-Host -ForegroundColor Green "Start OSDCloud"
+Start-OSDCloud -ZTI -OSVersion 'Windows 11' -OSBuild 23H2 -OSEdition Enterprise -OSLanguage en-us -OSLicense Retail
+
+Write-Host -ForegroundColor Green "Restarting in 20 seconds"
+Start-Sleep -Seconds 20
+wpeutil reboot
